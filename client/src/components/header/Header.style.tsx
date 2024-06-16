37d641c2002ec739824fc8ea8ff3545d5867619f
Davis <davisdjaja20@gmail.com>
2024-06-16 15:37:55 +1200

    added react router dom & styled component packages, added header and home directories & initial component test

---
+++ client/src/components/header/Header.style.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  height: 90px;
+  width: 100%;
+  display: flex;
+  background-color: #86796e;
+`;
---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer } from "./Header.style";
+
+export default function Header() {
+  return (
+    <>
+      <HeaderContainer>Header</HeaderContainer>
+    </>
+  );
+}

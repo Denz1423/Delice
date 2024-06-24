6eb234a58c40088980783e31f3644ec49d1f6e1e
Davis <davisdjaja20@gmail.com>
2024-06-24 18:50:29 +1200

    migrate server to dotnet 8 & initial db setup

---
+++ server/Entities/Product.cs
@@ -0,0 +1,11 @@
+namespace server.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string Type { get; set; }
+    }
+}
--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff06c19ed903c3a8ebe4c3fc207bafbb829c3ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
